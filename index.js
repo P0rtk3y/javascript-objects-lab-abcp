9c648580c6fdd151b7a2687da09bb0277b6ee7ad
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-18 06:09:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function deleteFromObjectByKey(object, key){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObj = Object.assign({}, object)
-  delete newObj[key]
-  return newObj
+  delete object[key]
+  return object
 }
